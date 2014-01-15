9a344158adc374c0c7b91fbcf04ffe6f9dce89b5
ObservatorioLesiones <observatorio.lesiones.tj@gmail.com>
2014-01-14 17:49:55 -0800

    modificacion en los modelos

--- observatorio_system/observatorio/models.py
+++ observatorio_system/observatorio/models.py
@@ -3,95 +3,76 @@ from django.db import models
 # Create your models here.
 
 class Municipio(models.Model):
-	id=models.AutoField(primary_key=True)
 	nombre_municipio=models.CharField(max_length=25)
-	
-
 	def __unicode__(self):
 		return self.nombre_municipio
 
 class Ciudad(models.Model):
-	id = models.AutoField(primary_key=True)
 	nombre_ciudad=models.CharField(max_length=25)
-	nombre_municipio=models.ForeignKey(Municipio)
+	municipio=models.ForeignKey(Municipio)
 	def __unicode__(self):
 		return self.nombre_ciudad
 
 class Delegacion(models.Model):
-	id=models.AutoField(primary_key=True)
-	nombre_d=models.CharField(max_length=25)
-	nombre_municipio=models.ForeignKey(Municipio)
-
+	nombre_delegacion=models.CharField(max_length=25)
+	municipio=models.ForeignKey(Municipio)
 	def __unicode__(self):
 		return self.nombre_d
-		
+
 class Condicion_Vial(models.Model):
-	id=models.AutoField(primary_key=True)
 	condicion_vial=models.CharField(max_length=30)
 
 	def __unicode__(self):
 		return self.condicion_vial
 
 class Descripcion_Vial(models.Model):
-	id=models.AutoField(primary_key=True)
 	descripcion=models.CharField(max_length=55)
 
 	def __unicode__(self):
-		return self.descripcion	
+		return self.descripcion
 
 
 class num_carriles(models.Model):
-	id=models.AutoField(primary_key=True)
 	num_carriles=models.CharField(max_length=30)
-	
 	def __unicode__(self):
 		return self.num_carriles
 
 class dispositivo_funcionando(models.Model):
-	id=models.AutoField(primary_key=True)
 	funcionando=models.CharField(max_length=40)
-
 	def __unicode__(self):
 		return self.funcionando
 
 class dispositivo_control_vial(models.Model):
-	id=models.AutoField(primary_key=True)
 	dispositivo_control=models.CharField(max_length=55)
-	
 	def __unicode__(self):
 		return self.dispositivo_control
 
 
 class condicion_luz(models.Model):
-	id=models.AutoField(primary_key=True)
 	condicion_luz=models.CharField(max_length=35)
 	
 	def __unicode__(self):
 		return self.condicion_luz
 
 class zona_construccion(models.Model):
-	id=models.AutoField(primary_key=True)
 	descripcion=models.CharField(max_length=35)
 
 	def __unicode__(self):
 		return self.descripcion
 
 class condicion_atmosferica(models.Model):
-	id=models.AutoField(primary_key=True)
 	condicion=models.CharField(max_length=25)
 
 	def __unicode__(self):
 		return self.condicion
 
 class metodo_transporte(models.Model):
-	id=models.AutoField(primary_key=True)
 	metodo=models.CharField(max_length=45)
 
 	def __unicode__(self):
 		return self.metodo
 
 class jurisdiccion_especial(models.Model):
-	id=models.AutoField(primary_key=True)
 	jurisdiccion=models.CharField(max_length=25)
 
 	def __unicode__(self):
@@ -130,14 +111,12 @@ class evento(models.Model):
 		return unicode(self.num_evento)
 
 class marca(models.Model):
-	id=models.AutoField(primary_key=True)
 	marca=models.CharField(max_length=40)
 
 	def __unicode__(self):
 		return self.marca
 
 class modelo(models.Model):
-	id=models.AutoField(primary_key=True)
 	marca=models.ForeignKey(marca)
 	modelo=models.CharField(max_length=30)
 	
@@ -145,14 +124,12 @@ class modelo(models.Model):
 		return self.modelo
 
 class camion_pasajeros(models.Model):
-	id=models.AutoField(primary_key=True)
 	tipo_camion=models.CharField(max_length=40)
 
 	def __unicode__(self):
 		return self.tipo_camion
 
 class volcadura(models.Model):
-	id=models.AutoField(primary_key=True)
 	volcadura=models.CharField(max_length=45)
 
 	def __unicode__(self):
@@ -168,7 +145,7 @@ class vehiculo(models.Model):
 	num_muertos=models.IntegerField()
 	conductor_alcoholizado=models.BooleanField()
 	num_lesionados=models.PositiveIntegerField()
-	num_evento=models.ForeignKey(evento)
+	evento=models.ForeignKey(evento)
 	@models.permalink
 	def get_absolute_url(self):
 		return ('observatorio.views.ver_vehiculo',(str(self.num_vehiculo),))
@@ -177,65 +154,55 @@ class vehiculo(models.Model):
 		return unicode(self.num_vehiculo)
 
 class rol_persona(models.Model):
-	id=models.AutoField(primary_key=True)
 	rol=models.CharField(max_length=45)
 
 	def __unicode__(self):
 		return self.rol
 
 class gravedad_lesion(models.Model):
-	id=models.AutoField(primary_key=True)
 	gravedad=models.CharField(max_length=35)
 
 	def __unicode__(self):
 		return self.gravedad
 
 class medidas_seguridad(models.Model):
-	id=models.AutoField(primary_key=True)
 	medida=models.CharField(max_length=45)
 
 	def __unicode__(self):
 		return self.medida
 
 class muerte_escena(models.Model):
-	id=models.AutoField(primary_key=True)
 	descripcion=models.CharField(max_length=20)
 
 	def __unicode__(self):
 		return self.descripcion
 
 class ocupacion(models.Model):
-	id=models.AutoField(primary_key=True)
 	ocupacion=models.CharField(max_length=50)
 
 	def __unicode__(self):
 		return self.ocupacion
 
 class estado_civil(models.Model):
-	id=models.AutoField(primary_key=True)
 	estado_civil=models.CharField(max_length=15)
 
 	def __unicode__(self):
 		return self.estado_civil
 
 class escolaridad(models.Model):
-	id=models.AutoField(primary_key=True)
 	escolaridad=models.CharField(max_length=20)
 
 	def __unicode__(self):
 		return self.escolaridad
 
 class prioridad(models.Model):
-	id=models.AutoField(primary_key=True)
 	prioridad=models.CharField(max_length=15)
 
 	def __unicode__(self):
 		return self.prioridad
 
 class persona(models.Model):
-	
 	sexo_choices=((0, 'Femenino'),(1, 'Masculino'))
-	id_persona=models.AutoField(primary_key=True,blank=True)
 	edad=models.IntegerField(null=True,blank=True)
 	sexo=models.IntegerField(choices=sexo_choices,null=True, blank=True)
 	rol=models.ForeignKey(rol_persona)
@@ -244,10 +211,10 @@ class persona(models.Model):
 	num_amb=models.CharField(max_length=2, null=True,blank=True)
 	operador=models.CharField(max_length=50, null=True,blank=True)
 	prest_serv=models.CharField(max_length=50, null=True, blank=True)
-	num_evento=models.ForeignKey(evento)
-	num_vehiculo=models.ForeignKey(vehiculo, null=True,blank=True)
-	clasificacion1=models.BooleanField()
-	clasificacion2=models.BooleanField()
+	evento=models.ForeignKey(evento)
+	vehiculo=models.ForeignKey(vehiculo, null=True,blank=True)
+	clasificacion1=models.BooleanField(null=True,blank=True)
+	clasificacion2=models.BooleanField(null=True,blank=True)
 	medidas_seguridad=models.ForeignKey(medidas_seguridad)
 	muerte_escena=models.ForeignKey(muerte_escena)
 	ocupacion=models.ForeignKey(ocupacion)
@@ -256,95 +223,85 @@ class persona(models.Model):
 	prioridad=models.ForeignKey(prioridad)
 
 	def __unicode__(self):
-		return unicode(self.id_persona)
+		return unicode(self.id)
 
 class tipo_licencia(models.Model):
-	id=models.AutoField(primary_key=True)
 	tipo=models.CharField(max_length=30)
 
 	def __unicode__(self):
 		return self.tipo
 
 class conductor_alcoholizado(models.Model):
-	id=models.AutoField(primary_key=True)
 	alcoholizado=models.CharField(max_length=35)
 
 	def __unicode__(self):
 		return self.alcoholizado
 
 class posicion_persona(models.Model):
-	id=models.AutoField(primary_key=True)
 	posicion=models.CharField(max_length=50)
 	def __unicode__(self):
 		return self.posicion
 
 class tipo_accidente(models.Model):
-	id=models.AutoField(primary_key=True)
 	tipo=models.CharField(max_length=30)
 
 	def __unicode__(self):
 		return self.tipo
 
 class interseccion_accidente(models.Model):
-	id=models.AutoField(primary_key=True)
 	interseccion=models.CharField(max_length=13)
 
 	def __unicode__(self):
 		return self.interseccion 
 
 class escenario_impacto(models.Model):
-	id=models.AutoField(primary_key=True)
 	escenario=models.CharField(max_length=100)
 	
 	def __unicode__(self):
 		return self.escenario
 
 class tipo_impacto(models.Model):
-	id=models.AutoField(primary_key=True)
 	tipo_impacto=models.CharField(max_length=50)
 
 	def __unicode__(self):
 		return self.tipo_impacto
 
 class ubicacion_impacto(models.Model):
-	id=models.AutoField(primary_key=True)
 	ubicacion=models.CharField(max_length=20)
 
 	def __unicode__(self):
 		return self.ubicacion
 
 class posicion_transeunte(models.Model):
-	id=models.AutoField(primary_key=True)
 	posicion=models.CharField(max_length=30)
 	def __unicode__(self):
 		return self.posicion
 
 class direccion_inicial(models.Model):
-	id=models.AutoField(primary_key=True)
 	direccion=models.CharField(max_length=30)
 	def __unicode__(self):
 		return self.direccion
 
 class conductor(models.Model):
-	id_persona=models.OneToOneField(persona, primary_key=True)
+	persona=models.OneToOneField(persona, primary_key=True)
 	licencia=models.BooleanField()
 	num_licencia=models.IntegerField()
 	tipo_licencia=models.ForeignKey(tipo_licencia)
 	conductor_alcoholizado=models.ForeignKey(conductor_alcoholizado)
 
 	def __unicode__(self):
-		return unicode(self.id_persona)
+		return unicode(self.persona)
 	
 class pasajero(models.Model):
-	id_persona=models.OneToOneField(persona, primary_key=True)
+	persona=models.OneToOneField(persona, primary_key=True)
 	posicion_persona=models.ForeignKey(posicion_persona)
 
 	def __unicode__(self):
-		return unicode(self.id_persona)
+		return unicode(self.persona)
 
 
 class transeunte(models.Model):
-	id_persona=models.OneToOneField(persona, primary_key=True)
+	persona=models.OneToOneField(persona, primary_key=True)
 	marca_crucero_peatonal=models.BooleanField()
 	banqueta=models.BooleanField()
 	zona_escolar=models.BooleanField()
@@ -358,5 +315,5 @@ class transeunte(models.Model):
 	
 
 	def __unicode__(self):
-		return unicode(self.id_persona)
+		return unicode(self.persona)
 	
